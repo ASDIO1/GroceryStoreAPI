e99bdefb5474e4022512a98096182b81cad35a92
ASDIO1 <diego.an.sanchez.a@gmail.com>
2021-03-22 16:14:08 -0400

    Created a controller endpoint and changed default URL

--- GroceryStoreAPI/GroceryStoreAPI/Controllers/FoodsController.cs
+++ GroceryStoreAPI/GroceryStoreAPI/Controllers/FoodsController.cs
@@ -1,11 +1,19 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace GroceryStoreAPI.Controllers
 {
-    public class FoodsController
+    [Route("api/foods")]
+    public class FoodsController : Controller
     {
+        [HttpGet]
+        public IEnumerable<string> GetFoods()
+        {
+            var foodList = new List<string>() { "Candy", "Fruits" };
+            return foodList;
+        }
     }
 }
