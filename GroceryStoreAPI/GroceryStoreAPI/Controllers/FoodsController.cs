a423d0399fd2c78064b8bf71ae564418fc280213
ASDIO1 <diego.an.sanchez.a@gmail.com>
2021-03-23 22:52:50 -0400

    First Controller and Model Working
    This is a first step towards building this API with Monolithic Architecture.

--- GroceryStoreAPI/GroceryStoreAPI/Controllers/FoodsController.cs
+++ GroceryStoreAPI/GroceryStoreAPI/Controllers/FoodsController.cs
@@ -7,32 +7,50 @@ using System.Threading.Tasks;
 
 namespace GroceryStoreAPI.Controllers
 {
-    [Route("api/foods")]
+    [Route("api/[controller]")]
     public class FoodsController : Controller
     {
-        private IList<FoodModel> _foods;
-
-        public FoodsController()
+        static private IList<FoodModel> _foods = new List<FoodModel>() 
         {
-            _foods = new List<FoodModel>();
-            _foods.Add(new FoodModel()
-            {
-                Id = 1,
-                name = "Candy",
-                description = "A world of candies you cant even imagine"
-            });
-            _foods.Add(new FoodModel()
+            new FoodModel()
+                {
+                    Id = 1,
+                    name = "Candy",
+                    description = "A world of candies you cant even imagine"
+                },
+            new FoodModel()
             {
                 Id = 2,
                 name = "Meat",
                 description = "Delicious meat for all tastes"
-            });
+            }
+        };
+
+        public FoodsController()
+        {
         }
 
         [HttpGet]
         public IEnumerable<FoodModel> GetFoods()
         {
             return _foods;
+        }
+
+        [HttpGet("{foodId:long}")]
+        public FoodModel GetFood(long foodId)
+        {
+            var food = _foods.FirstOrDefault(f => f.Id == foodId);
+            return food;
+        }
+
+        // POST   api/foods
+        [HttpPost]
+        public FoodModel PostFood([FromBody] FoodModel newFood)
+        {
+            var next_Id = _foods.OrderByDescending(f => f.Id).FirstOrDefault().Id + 1;
+            newFood.Id = next_Id;
+            _foods.Add(newFood);
+            return newFood;
         }
     }
 }
